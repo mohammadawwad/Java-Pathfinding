6192ab3f3d85bad0ff7f7ad7684d943457fda71c
mohammadawwad <mohd-awwad@hotmail.com>
2021-09-02 19:04:09 -0700

    ugly print

--- Pathfinding/src/FRCVersion/FrcApp.java
+++ Pathfinding/src/FRCVersion/FrcApp.java
@@ -28,6 +28,9 @@ import javax.swing.JSlider;
 import javax.swing.JTextField;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import org.json.simple.JSONObject;
+import java.util.LinkedHashMap;
+import org.json.simple.JSONArray;
 
 public class FrcApp {
 
@@ -254,8 +257,47 @@ public class FrcApp {
                     //Write JSON file
                     try (FileWriter file = new FileWriter(pathFile)) {
 
-                        // file.write(coordinates); 
+                        // creating JSONObject
+                        JSONObject jo = new JSONObject();
+                        // putting data to JSONObject
+                        jo.put("firstName", "John");
+                        jo.put("lastName", "Smith");
+                        jo.put("age", 25);
+
+                        // for address data, first create LinkedHashMap
+                        LinkedHashMap m = new LinkedHashMap(4);
+                        m.put("streetAddress", "21 2nd Street");
+                        m.put("city", "New York");
+                        m.put("state", "NY");
+                        m.put("postalCode", 10021);
+
+                        // putting address to JSONObject
+                        jo.put("address", m);
+                        
+                        // for phone numbers, first create JSONArray 
+                        JSONArray ja = new JSONArray();
+                        
+                        m = new LinkedHashMap(2);
+                        m.put("type", "home");
+                        m.put("number", "212 555-1234");
+                        
+                        // adding map to list
+                        ja.add(m);
+                        
+                        m = new LinkedHashMap(2);
+                        m.put("type", "fax");
+                        m.put("number", "212 555-1234");
+                        
+                        // adding map to list
+                        ja.add(m);
+                        
+                        // putting phoneNumbers to JSONObject
+                        jo.put("phoneNumbers", ja);
+                        final String inputJson = "{\"one\":\"AAA\",\"two\":[\"BBB\",\"CCC\"],\"three\":{\"four\":\"DDD\",\"five\":[\"EEE\",\"FFF\"]}}";
+
+                        System.out.println(new JSONObject().toString(4));
                         file.flush();
+                        file.close();
                     } 
 
                 } catch (IOException e1) {
